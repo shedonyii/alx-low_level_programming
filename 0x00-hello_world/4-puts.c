97604d781bc27f8174cabff0f3df026cc8ca5626
shedonyii <shedonyii@gmail.com>
2022-10-13 15:05:37 -0700

    6-size.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
-/**
- *main- serves as the entry point to the program
- *
- *Return: By default returns zero if no erros otherwise returns non-zero value.
- */
-int main(void)
-{
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
-}    
+#include <stdio.h>                                                                                                                    
+/**                                                                                                                                   
+ *  * main-entry point for the program.                                                                                                  
+ *   *   **Return:0 if no errors,return non zero if errors                                                                                
+ *    */                                                                                                                                   
+int main(void)                                                                                                                        
+{                                                                                                                                     
+	                puts("\"Programming is like building a multilingual puzzle");                                                  
+			                        return (0);                                                                                                   
+}
+
 
 
 
