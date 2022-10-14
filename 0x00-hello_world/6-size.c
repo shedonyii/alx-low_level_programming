ef07820158aa1036cf2331d0de0b1973ef1e870e
shedonyii <shedonyii@gmail.com>
2022-10-14 00:23:25 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,8 +2,8 @@
 /**
  *  *main -is the entry point for the program.
  *   *
- *    *Return:zero if no error, Non-zero value  if errors.
- *     */
+ *    *Return:zero if no error, Non-zero value  if errors. 
+ */
 int main(void)
 {	
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
