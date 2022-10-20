bb775e8ccbf8e76c6fedcda7bcb6748afbe780b8
shedonyii <shedonyii@gmail.com>
2022-10-20 13:00:09 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *  ** print_line - prints underscores
+ *    *   * @n: number of underscores
+ *      *    * Return: values
+ **/
+
+void print_line(int n)
+
+{
+
+	int line;
+
+
+	for (line = 0; line < n; line = line + 1)
+
+
+	{
+
+
+		_putchar('_');
+
+
+	}
+
+
+	_putchar('\n');
+
+}
