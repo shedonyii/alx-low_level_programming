d1011220bb1ab1bca134606e47cc36b2a210df72
shedonyii <shedonyii@gmail.com>
2022-10-20 13:12:31 -0700

    8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,49 @@
+i#include "main.h"
+/**
+ *  ** print_square - prints square
+ *    *   * @size: parameter
+ *      *    * Return: size of the square
+ **/
+void print_square(int size)
+
+{
+
+	int hash1;
+
+	int hash2;
+
+	if (size > 0)
+
+	{
+
+		for (hash1 = 0; hash1 < size; hash1++)
+
+
+		{
+
+			for (hash2 = 1; hash2 < size; hash2++)
+
+
+
+			{
+
+				_putchar('#');
+
+			}
+
+			_putchar('#');
+
+			_putchar('\n');
+
+		}
+
+	}
+
+	else
+
+	{
+
+		_putchar('\n');
+	}
+
+}
