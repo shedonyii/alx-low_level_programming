89fa4f04c1f503d5c1ee9907fff5742dd5f5ff12
shedonyii <shedonyii@gmail.com>
2022-10-24 09:12:00 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+
+
+
+
+
+/**
+ *  ** swap_int - swaps positions of the parameters
+ *   ** @a: first parameter
+ *    ** @b: second parameter
+ **/
+
+void swap_int(int *a, int *b)
+
+{
+
+	int num = *a;
+	*a = *b;
+	*b = num;
+}
