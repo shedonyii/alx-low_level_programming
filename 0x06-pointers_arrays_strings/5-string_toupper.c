5bd308130c52656910dc16ae0d22bb4414f3125f
shedonyii <shedonyii@gmail.com>
2022-10-26 06:57:43 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *  ** string_toupper - changes all lowercase letters of a string to uppercase
+ *    *   * @upper: input for conversion of lowercase to uppercase
+ *      *    * Return: uppercase
+ **/
+
+char *string_toupper(char *upper)
+
+{
+
+	int i;
+
+	for (i = 0; upper[i] != '\0'; i++)
+
+	{
+
+		if (upper[i] >= 'a' && upper[i] <= 'z')
+
+		{
+
+			upper[i] = upper[i] - 32;
+
+		}
+
+	}
+
+	return (upper);
+}
