85e41ddc6e8846f7569c67b854fc603cecb76834
shedonyii <shedonyii@gmail.com>
2022-11-16 10:57:11 -0800

    1-array_iterator.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,22 +1,17 @@
-#include <stdlib.h>
-
 #include "function_pointers.h"
 
-
 /**
- *  * print_name - prints a name
- *  * @name: name to print
- *    * @f: pointer to the printing function
+ *  ** print_name - prints a name
+ *   ** @name: name to print
+ *    ** @f: function to print with
+ *      ** Return: void
  **/
 
 void print_name(char *name, void (*f)(char *))
 
 {
 
-		if (!name || !f)
+		if (name != NULL && f != NULL)
 
-					return;
-
-			f(name);
-
+					f(name);
 }
